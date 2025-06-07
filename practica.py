f4010b60dd57e49338da3df78ed82ab6753f3b76
Mateo <mateonehemias554@gmail.com>
2025-06-07 01:08:30 -0500

    Mejoras: actualicé funcionalidades y optimicé código

--- practica.py
+++ practica.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-import numpy as np
-import math
+#la mision de todo esto es calcular el seno de X para un  
+import numpy as np # se importa la libreria numpy para hacer rapidos calculos matematicos
+import math # 
 
-# Create random input and output data
-x = np.linspace(-math.pi, math.pi, 2000)
-y = np.sin(x)
+# Crear valores aleatorios  de inputs (valores asignados ala maquina para que lo procese) y output (resultado de la maquina)
+x = np.linspace(-math.pi, math.pi, 2000)  #X es el output 
+y = np.sin(x) #Y es el input 
 
-# Randomly initialize weights
+# Pesos aleatorios de para (a,b,c,d)
 a = np.random.randn()
 b = np.random.randn()
 c = np.random.randn()
 d = np.random.randn()
 
-learning_rate = 1e-6
-for t in range(2000):
-    # Forward pass: compute predicted y
+learning_rate = 1e-6 #indice de aprendizaje del modelo tiene que ser un numero muy pequeño para que el modelo no se sobre ajuste
+for t in range(2000): #2000 es el numero de iteraciones que se van a hacer para que el modelo se ajuste
+    # Predecir los valores para Y
     # y = a + b x + c x^2 + d x^3
     y_pred = a + b * x + c * x ** 2 + d * x ** 3
 
-    # Compute and print loss
+    # Funcion de perdida (cuanto se aleja el resultado del modelo del resultado real)
     loss = np.square(y_pred - y).sum()
     if t % 100 == 99:
         print(t, loss)
@@ -28,12 +28,14 @@ for t in range(2000):
     grad_a = grad_y_pred.sum()
     grad_b = (grad_y_pred * x).sum()
     grad_c = (grad_y_pred * x ** 2).sum()
-    grad_d = (grad_y_pred * x ** 3).sum()
+    grad_d = (grad_y_pred * x ** 3).sum() 
+    #lo de arriba es el descenso del gradiente para que el modelo se ajuste a los datos
 
-    # Update weights
+
+    # Pesos actualizados 
     a -= learning_rate * grad_a
     b -= learning_rate * grad_b
     c -= learning_rate * grad_c
     d -= learning_rate * grad_d
 
-print(f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3')
+print(f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3') #imprime todo el proceso ya mencionado 
